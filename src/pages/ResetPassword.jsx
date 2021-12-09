e260ecc37efa787327b858cf468f3ffee4842f9b
codeddave <drizzzydave@gmail.com>
2021-12-10 00:48:17 +0100

    building reset password form

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,9 +1,16 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
 
 const ResetPassword = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Formik>
+        {({}) => (
+          <Form>
+            <Field></Field>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };

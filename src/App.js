1c5fa41f8d7f11e653edae78df952e448789dd15
codeddave <drizzzydave@gmail.com>
2021-12-10 00:36:43 +0100

    adding reset password page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ResetPassword from "./pages/ResetPassword";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

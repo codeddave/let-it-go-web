a0fd596f0f4743a9930e55b8cd1aafcbca9eeadc
codeddave <drizzzydave@gmail.com>
2021-12-20 09:43:27 +0100

    update

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,9 +1,15 @@
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { resetPasssword } from "../api/resetPassword";
 import LetItGoLogo from "../assets/images/let-it-go.jpg";
 import "./ResetPassword.css";
 
 const ResetPassword = () => {
+  const { token } = useParams();
+  const handleSubmit = (values) => {
+    resetPasssword(values.password, token);
+  };
   return (
     <div className="reset-page">
       <section>
@@ -14,12 +20,15 @@ const ResetPassword = () => {
           password: "",
           confirmPassword: "",
         }}
+        onSubmit={handleSubmit}
       >
-        {({}) => (
+        {({ errors, touched }) => (
           <Form className="reset-form">
             <Field name="password" placeholder="password" />
             <Field name="confirmPassword" placeholder="confirm password" />
-            <button className="reset-button">Reset Password</button>
+            <button type="submit" className="reset-button">
+              Reset Password
+            </button>
           </Form>
         )}
       </Formik>

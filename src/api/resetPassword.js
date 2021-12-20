429b7c97fd73b1d84df5a09e17d6fc69348fb4d8
codeddave <drizzzydave@gmail.com>
2021-12-20 09:48:27 +0100

    updated api url

--- src/api/resetPassword.js
+++ src/api/resetPassword.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const resetPasssword = async (password, token) => {
   try {
-    await axios.post(`https://let-itgo.herokuapp.com/${token}`, { password });
+    await axios.post(`https://let-itgo.herokuapp.com/reset-password/${token}`, {
+      password,
+    });
   } catch (error) {
     console.log(error);
   }

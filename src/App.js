58d4effac6de738ef567d5054dd0aabd396e7385
codeddave <drizzzydave@gmail.com>
2021-12-20 16:58:52 +0100

    added react toastify

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/reset-password/:token"
-            element={<ResetPassword />}
-          />
-        </Routes>
-      </Router>
-    </div>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/reset-password/:token"
+              element={<ResetPassword />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
 }
 
--- src/api/resetPassword.js
+++ src/api/resetPassword.js
@@ -1,11 +1,16 @@
 import axios from "axios";
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export const resetPasssword = async (password, token) => {
   try {
-    await axios.post(`https://let-itgo.herokuapp.com/reset-password/${token}`, {
-      password,
-    });
+    await axios.post(
+      `https://let-itgo.herokuapp.com/user/reset-password/${token}`,
+      {
+        password,
+      }
+    );
   } catch (error) {
+    toast.error("something went wrong");
     console.log(error);
   }
 };
